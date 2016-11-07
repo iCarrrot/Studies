8b0871407daae3a0e5b03629ce767cf06eb876a5
iCarrrot <michal.mar3@gmail.com>
2016-11-07 16:10:13 +0100

    zadania

--- ANM/Projects/P1/fcja.jl
+++ ANM/Projects/P1/fcja.jl
@@ -1,15 +1,18 @@
-setprecision(3000)
+setprecision(200)
+Pkg.add("PyPlot")
+using PyPlot
+x = linspace(0,2*pi,1000); y = sin(3*x + 4*cos(2*x))
+plot(x, y, color="red", linewidth=2.0, linestyle="--")
+
+f1(x)=convert(typeof(x),7)* x*x
+f2(x)=x*x - convert(typeof(x),4)
 
 
-f(1,x)=convert(typeof(x),7)* x*x  
-f(2,x)=x*x - convert(typeof(x),4)        
-
-
-function steff(x,funkcja,numer,kroki)
+function steff(x,funkcja,kroki)
     i=0
     while i<kroki
-        f_x=funkcja(numer,x)
-        x1=(f_x*f_x)/(funkcja(numer,x+f_x)-f_x)
+        f_x=funkcja(x)
+        x1=(f_x*f_x)/(funkcja(x+f_x)-f_x)
         x=x1
         i+=1
         println(x)
@@ -17,4 +20,8 @@ function steff(x,funkcja,numer,kroki)
     x
 end
 zmienna=BigFloat(0.0)
-println(steff(zmienna,f,1,300))
+zmienna=zmienna+10*eps(typeof(zmienna))
+
+println(steff(zmienna,f2,3000))
+println("blah")
+println(zmienna)
---
+++ PF/l3/z2.ml
@@ -0,0 +1,71 @@
+(*z4*)
+let reverse x_list =
+	let rec rev_acc acc x_list = 
+	match x_list with
+	| x::xs -> rev_acc (x::acc) xs
+	| [] -> acc
+
+in 
+rev_acc [] x_list
+
+(*z2*)
+let scanl (f:'a->'b->'a) (a:'a) (b_list:'b list) : 'a list =
+	let rec aux f b_list (a::acc) =
+		match b_list with
+			|b::bs -> aux f bs ((f a b)::a::acc) 
+			|[]->reverse (a::acc) 
+	in
+	aux f b_list (a::[])
+
+
+
+
+
+(*z5*)
+let rec map (f:'a->'b) (x_list:'a list) : 'b list=
+	match x_list with
+		| x::xs -> (f x)::(map f xs)
+		| [] -> []
+(*z6*)
+let rec all (f: 'a -> bool) (a_list: 'a list) : bool =
+	match a_list with
+		|x::xs->(f x)&&(all f xs)
+		|[]-> true
+
+
+(*  z7+a) *)
+
+let rec any (f: 'a -> bool) (a_list: 'a list) : bool =
+	match a_list with
+		|x::xs->if f x then true else any f xs
+		|[]-> false 
+
+(*z8*)
+type aritm_literal = Neg | Add | Sub | Mul | Div | Pow | Num of float
+
+(*z9*)
+
+exception IllegalOperation of (aritm_literal* float	* float)
+exception TooFewOperations
+
+
+let eval_rpm (a_list:aritm_literal list) : float=
+	let operations operator arg1 arg2 : float=
+		match operator with 
+			| Add -> arg1+.arg2
+			| Sub -> arg1-.arg2
+			| Mul ->arg1*.arg2
+			| Div ->if arg2=0.0 then raise (IllegalOperation(operator,arg1,arg2)) else arg1/.arg2
+			| Pow -> try arg1**arg2 with _ -> raise (IllegalOperation(operator,arg1,arg2))
+			| _ -> raise (IllegalOperation(operator,arg1,arg2))
+	in
+		let rec aux a_list acc =
+			match a_list with
+			|  Num x ::xs-> aux xs (x::acc)
+			| Neg::xs -> let y::ys = acc in aux xs ((-1.0*.y)::ys)
+			|x::xs -> let y1::y2::ys =acc in aux xs ((operations x y2 y1)::ys)
+			|[]-> let y::ys=acc in if ys=[] then y else raise TooFewOperations
+		in
+			aux a_list []
+
+
