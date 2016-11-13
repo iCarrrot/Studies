42b552f45ef7550d0aa94e3d5f8f076fb0ae08d3
iCarrrot <michal.mar3@gmail.com>
2016-11-13 23:46:07 +0100

    nie cierpię numerków

--- ANM/Projects/P1/t2.jl.jl
+++
@@ -1,5 +0,0 @@
-using PyPlot
-x = linspace(0, 10, 200)
-y = sin(x)
-fig, ax = subplots()
-ax[:plot](x, y, "b-", linewidth=2)
--- ANM/Projects/P1/zadanie/prog/program.jl
+++ ANM/Projects/P1/zadanie/prog/program.jl
@@ -1,19 +1,14 @@
 using PyPlot
-
+using Match
 setprecision(300)
     
-f1(x)=convert(typeof(x),7)* x*x
-f3(x)=convert(typeof(x),10)*log10(x)
-f4(x)=x*x*x-convert(typeof(x),5)*x*x+convert(typeof(x),3)*x-convert(typeof(x),7)
-f5(x)=x-tan(x)
-f6(x)=x*x-convert(typeof(x),15)*x+convert(typeof(x),50)
+
 
 function newton(x,funkcja,nr,kroki)
     i=0
     (f_x,df_x)=funkcja(x,nr)
     while i<kroki
         (f_x,df_x)=funkcja(x,nr)
-       # println(funkcja(x,nr))
         x=x-f_x/df_x
         i+=1
     end
@@ -25,15 +20,10 @@ end
 function steff(x,funkcja,nr,kroki)
     i=0
     (f_x,df_x)=funkcja(x,nr)
-    #println("bbbbbbbbbbbbbbbbb")
     while i<kroki
-        #println("aaaaaaa")
         (f_x,df_x)=funkcja(x,nr)
-       # println(f_x)
         (f2_x,df2_x)=funkcja(x+f_x,nr)
-       # println(f2_x)
         last_x=x
-       # println(( f_x * f_x )/(f2_x-f_x))
         x=x-( f_x * f_x )/(f2_x-f_x)
         if (isnan(x)||isinf(x))
             #println("WARNING!!","  f_x ",f_x,"  x ",x,"  last_x ",last_x)
@@ -41,56 +31,32 @@ function steff(x,funkcja,nr,kroki)
 
             break
         end
-       # println("i  ",i,)
-       # println("x  ",x)
-       # println("f_x  ",f_x)
-        i+=1
-        
+        i+=1 
     end
- 
     (x,f_x,-log10(abs(f_x-convert(typeof(x),0.0)) ) )
 
 end
-#=
+
 function steff(x,funkcja,kroki)
     i=0
-    
-    #println("bbbbbbbbbbbbbbbbb")
+
     while i<kroki
-        #println("aaaaaaa")
+
         f_x=funkcja(x)
         last_x=x
-
         x=x-(f_x*f_x)/(funkcja(x+f_x)-f_x)
         if (isnan(x)||isinf(x))
-            println("WARNING!!","  f_x ",f_x,"  x ",x,"  last_x ",last_x)
             return last_x
-
             break
         end
-       # println("i  ",i,)
-       # println("x  ",x)
-       # println("f_x  ",f_x)
         i+=1
         
     end
- 
     (x,f_x,-log10(abs(f_x-convert(typeof(x),0.0)) ) )
 
 end
-=#
-zmienna=BigFloat(0.0)
-zmienna=zmienna+10*eps(typeof(zmienna))
 
-
-
-#println(steff(zmienna,f2,3000))
-
-#println("blah")
-#println(zmienna)
-
-
-
+#funkcja sprawdzająca zbieżność funkcji
 function check( typ,f ,nr, m ,m1,krok,border)
     if(typ=="steff")
         plotx1=[]
@@ -110,11 +76,9 @@ function check( typ,f ,nr, m ,m1,krok,border)
         while m<m01+border*krok
             (x1,f_x,_)=steff(m,f,nr,100)
             if abs(f_x)<convert(typeof(x1), 0.001)
-
                 push!(plotx1,m)
                 push!(plotyx,x1)
                 push!(ploty1,3.0)
-                #println(x1,"  ",x1-x0)
 
             else
                 push!(plotx2,m)
@@ -122,7 +86,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
 
             end
             m+=convert(typeof(x1), krok)
-            #println("m  ", m,"  x  ",x1)
         end
         i=0
         m=m0
@@ -140,8 +103,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
                     push!(ploty2,-2.0)
             end
             m-=convert(typeof(x1), krok/convert(typeof(krok), 10))
-            #println("m  ", m,"  x  ",x1)
-            #print("i: ",i)
         end
         i=0
         m=m0
@@ -151,15 +112,12 @@ function check( typ,f ,nr, m ,m1,krok,border)
                 push!(plotyx,x1)
                 push!(plotx1,m)
                 push!(ploty1,1.0)
-                #println(x1,"  ",x1-x0)
-
             else
                 push!(plotx2,m)
                 push!(ploty2,-1.0)
 
             end
             m-=convert(typeof(x1), krok)
-            #println("m  ", m,"  x  ",x1)
         end
     elseif(typ=="newton")
         plotx1=[]
@@ -182,7 +140,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
                 push!(plotyx,x1)
                 push!(plotx1,m)
                 push!(ploty1,3.0)
-                #println(x1,"  ",x1-x0)
 
             else
                 push!(plotx2,m)
@@ -190,7 +147,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
 
             end
             m+=convert(typeof(x1), krok)
-            #println("m  ", m,"  x  ",x1)
         end
         i=0
         m=m0
@@ -208,8 +164,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
                     push!(ploty2,-2.0)
             end
             m-=convert(typeof(x1), krok/convert(typeof(krok), 10))
-            #println("m  ", m,"  x  ",x1)
-            #print("i: ",i)
         end
         i=0
         m=m0
@@ -219,7 +173,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
                 push!(plotyx,x1)
                 push!(plotx1,m)
                 push!(ploty1,1.0)
-                #println(x1,"  ",x1-x0)
 
             else
                 push!(plotx2,m)
@@ -227,7 +180,6 @@ function check( typ,f ,nr, m ,m1,krok,border)
 
             end
             m-=convert(typeof(x1), krok)
-            #println("m  ", m,"  x  ",x1)
         end
     end
 
@@ -309,8 +261,8 @@ function dokladnosc(n,nr,x0,lebel)
 
 
     for i in 1:(n+1)
-        (xn,fxn,εn)=newton(x0,poly_function,nr,i)
-        (xs,fxs,εs)=steff(x0,poly_function,nr,i)
+        (xn,fxn,εn)=newton(x0,non_linear_f,nr,i)
+        (xs,fxs,εs)=steff(x0,non_linear_f,nr,i)
         εn=convert(Float64,εn)
         εs=convert(Float64,εs)
         xn=convert(Float64,xn)
@@ -323,12 +275,15 @@ function dokladnosc(n,nr,x0,lebel)
 
     end
 
-    subplot(211)
-    suptitle(string("Dokładnosci metod dla ",lebel, " i x0=",convert(Float64,x0)))
+    
+    subplot(111)
+    xlabel("Numer iteracji")
+    ylabel("-log10(f(x)")
+    title(string("Dokładnosci metod dla ",lebel, " i x0=",convert(Float64,x0)))
     plot(ax, newton_y, label="Newton", "blue",linewidth=1.0 ,marker="o")
     plot(ax, steff_y , "red", linewidth=1.0 ,marker="o",label="Steffensen")
     legend(loc="lower right",fancybox="true")
-
+    axis([0.0, (convert(Float64,n)+n/10),-1.0, 100.0])
     subplot(212)
     plot(ax, s_n_y , "green", linewidth=1.0 ,marker="o",label="Róznica")
     legend(loc="upper right",fancybox="true")
@@ -336,7 +291,11 @@ function dokladnosc(n,nr,x0,lebel)
 
 end
 function rozbieganie(krok,nr,border,x,x1,)
-
+    if x>x1
+        temp=x
+        x=x1
+        x1=temp
+    end
     wart_x=[convert(Float64,x),convert(Float64,x1)]
     wart_y=[0.0,0.0]
 
@@ -354,7 +313,7 @@ function rozbieganie(krok,nr,border,x,x1,)
     badn_x=[]
 
 
-    (goods_x,goods_y,z_pl,bads_x,bads_y)=check("steff",poly_function,nr,x,x1,krok,border)
+    (goods_x,goods_y,z_pl,bads_x,bads_y)=check("steff",non_linear_f,nr,x,x1,krok,border)
     goods_x=map(x->convert(Float64, x),goods_x)
     bads_x=map(x->convert(Float64, x),bads_x)
     goods_y=map(x->convert(Float64, x),goods_y)
@@ -363,7 +322,7 @@ function rozbieganie(krok,nr,border,x,x1,)
 
 
 
-    (goodn_x,goodn_y,z_pl,badn_x,badn_y)=check("newton",poly_function,nr,x,x1,krok,border)
+    (goodn_x,goodn_y,z_pl,badn_x,badn_y)=check("newton",non_linear_f,nr,x,x1,krok,border)
     goodn_x=map(x->convert(Float64, x),goodn_x)
     badn_x=map(x->convert(Float64, x),badn_x)
     goodn_y=map(x->convert(Float64, x),goodn_y)
@@ -407,7 +366,19 @@ function rozbieganie(krok,nr,border,x,x1,)
     plot(goods_x, zs_pl , "red", linewidth=0.0 ,marker="x")
 
 
-    #axis([ -100, 100,-3.5, 3.5,])
+end
+
+function make_tx_table(n,x0,funkcja,nr)
+    i=0
+    s=""
+    (f_x,_)=funkcja(x0,nr)
+    while i<n
+        (x,f_x,_)=steff(x0,funkcja,nr,i)
+        
+        s=string(s,i," & ",convert(Float64, x)," & ",convert(Float64, f_x)," \\\\","\n")
+        i+=10
+    end
+    print(s)
 
 end
 
@@ -417,39 +388,3 @@ end
 
 
 
-
-
-
-#=
-miejsce=BigFloat(7.7)
-miejsce1=BigFloat(4.5)
-(x1,y1,x11,y11,x2,y2)=check(f5,miejsce,miejsce1)
-=#
-
-#=
-miejsce=BigFloat(5.0)
-miejsce1=BigFloat(10.0)
-(x1,y1,x11,y11,x2,y2)=check(f6,miejsce,miejsce1)
-#println(x1,"/n")
-#println("end")
-#println(x2)
-x2=map(x->convert(Float64, x),x2)
-x1=map(x->convert(Float64, x),x1)
-y2=map(x->convert(Float64, x),y2)
-y1=map(x->convert(Float64, x),y1)
-x11=map(x->convert(Float64, x),x11)
-y11=map(x->convert(Float64, x),y11)
-=#
-
-
-
-#=
-
-
-plot(x1, y1, "blue",linewidth=0.0 ,marker="o")
-plot(x2, y2 , "red", linewidth=0.0 ,marker="o")
-plot(x11, y11 , "green", linewidth=0.0 ,marker="o")
-axis([ 3, 40,-2, 2])
-title("Wykres zbierzności")
-show()
-=#
